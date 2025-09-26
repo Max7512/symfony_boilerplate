f5d25f70e33afc70e5efbd9dc33189e43b0b11eb
Max7512 <hemmertmaxence@gmail.com>
2025-09-26 16:04:46 +0200

    avancement exercice 1

--- src/Controller/BurgerController.php
+++ src/Controller/BurgerController.php
@@ -11,6 +11,26 @@ class BurgerController extends AbstractController
     #[Route('/burgers', name: 'burgers')]
     public function list(): Response
     {
+        $burgers = [
+            [
+                "name" => "Cheeseburger",
+                "id" => 1
+            ],
+            [
+                "name" => "Baconator",
+                "id" => 2
+            ],
+            [
+                "name" => "Crispy chicken",
+                "id" => 3
+            ]
+        ];
         return $this->render('burgers_list.html.twig');
+    }
+
+    #[Route('/burgers/{id}', name: 'burger')]
+    public function burger(string $id): Response
+    {
+        return $this->render('burger.html.twig');
     }
 }
