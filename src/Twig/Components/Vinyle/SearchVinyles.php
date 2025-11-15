af451196ef85d9df06adfde618894426b2d86d02
hemmertmaxence <hemmertmaxence@gmail.com>
2025-11-15 23:42:58 +0000

    finalisation de la pagination des vinyles

--- src/Twig/Components/Vinyle/SearchVinyles.php
+++ src/Twig/Components/Vinyle/SearchVinyles.php
@@ -5,7 +5,7 @@ namespace App\Twig\Components\Vinyle;
 use App\Repository\VinyleRepository;
 use Knp\Component\Pager\Pagination\PaginationInterface;
 use Symfony\UX\LiveComponent\Attribute\AsLiveComponent;
-use Symfony\UX\LiveComponent\Attribute\LiveListener;
+use Symfony\UX\LiveComponent\Attribute\LiveAction;
 use Symfony\UX\LiveComponent\Attribute\LiveProp;
 use Symfony\UX\LiveComponent\DefaultActionTrait;
 
@@ -14,7 +14,7 @@ class SearchVinyles
 {
     use DefaultActionTrait;
 
-    #[LiveProp(writable: true, url: true)]
+    #[LiveProp(writable: true)]
     public ?string $search = null;
 
     #[LiveProp]
@@ -23,22 +23,39 @@ class SearchVinyles
     #[LiveProp(writable: true)]
     public int $pageLimit = 25;
 
-    public function __construct(private VinyleRepository $vinyleRepository)
-    {
-    }
+    #[LiveProp]
+    public int $pageCount = 1;
+
+    public function __construct(private VinyleRepository $vinyleRepository) {}
 
     public function getVinyles(): PaginationInterface
     {
         return $this->vinyleRepository->getAllPaginate($this->page, $this->search, $this->pageLimit);
     }
 
-    #[LiveListener('pagePlus')]
-    public function pagePlus(): void {
-        $this->page++;
+    #[LiveAction]
+    public function pagePlus(): void
+    {
+        if ($this->page < $this->pageCount)
+            $this->page++;
+
+        $this->refreshPageOptions();
     }
 
-    #[LiveListener('pageMoins')]
-    public function pageMoins(): void {
-        $this->page--;
+    #[LiveAction]
+    public function pageMoins(): void
+    {
+        if ($this->page > 1)
+            $this->page--;
+
+        $this->refreshPageOptions();
+    }
+
+    #[LiveAction]
+    public function refreshPageOptions(): void {
+        $this->pageCount = ceil($this->vinyleRepository->getAllPaginate($this->page, $this->search, $this->pageLimit)->getTotalItemCount() / $this->pageLimit);
+
+        if ($this->page > $this->pageCount)
+            $this->page = $this->pageCount;
     }
 }
